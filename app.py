5f30f507aef57b6ce7eb9d1938a8821ca8d0fca9
jay-linux <jaylinuxmumbai@gmail.com>
2025-03-20 08:14:57 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import json
+import boto3
+def lambda_handler(event, context):
+    clinet = boto3.client('ec2', region_name='ap-south-1')
+    response = ec2.run_instances(
+        ImageId='ami-0abcdef1234567890',  # Replace with a valid AMI ID
+        InstanceType='t2.micro',
+        KeyName='my-key-pair',
+        MinCount=1,
+        MaxCount=1
+    )
+  
